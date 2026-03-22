ff2fc764e04913ae094f5e608de8813d4a16b39d
laozidabo <145752528+laozidabo@users.noreply.github.com>
2026-03-22 20:01:40 +0800

    Update music file in MusicCard component

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -12,7 +12,7 @@ import { Pause } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 
-const MUSIC_FILES = ['/music/close-to-you.mp3']
+const MUSIC_FILES = ['/music/girl in red – we fell in love in october.mp3']
 
 export default function MusicCard() {
 	const pathname = usePathname()
